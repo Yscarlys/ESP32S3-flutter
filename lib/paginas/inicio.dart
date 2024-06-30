63c0ca2124ac2108fb71f5d123499eec591a6d7e
Yscarlys <yscarlysvanessa0208@gmail.com>
2024-06-30 17:39:22 -0400

    Holi soy el mensaje xoxo

--- lib/paginas/inicio.dart
+++ lib/paginas/inicio.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:lite_rolling_switch/lite_rolling_switch.dart';
 
 class Inicio extends StatelessWidget {
   const Inicio({super.key});
@@ -12,41 +14,155 @@ class Inicio extends StatelessWidget {
         children: <Widget>[
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Container(
-                width: 300.0,
-                height: 400.0,
-                color: const Color(0xff141b22),
-                margin:
-                  const EdgeInsets.symmetric(vertical: 40.0, horizontal: 0.0),
-                child: Column(
-                  children: <Widget>[
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Container(
-                          padding: const EdgeInsets.all(20.0),
-                          child: Text(
-                            'Temperatura'.toUpperCase(),
-                            style: const TextStyle(
-                              color: Color(0xFFF89411),
-                              fontSize: 36.0,
-                              fontWeight: FontWeight.w700
-                            ),
-                          ),
-                        ),
-                      ]
-                    )
-                  ],
-                ),
-              ),
-            ],
+            children: _cardTemperatura,
           ),
           Row(
-            children: <Widget>[],
-          )
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: _cardSwitches,
+          ),
         ],
       ),
     );
   }
+
+  List <Widget> get _cardTemperatura {
+    return <Widget>[
+            Container(
+              width: 300.0,
+              height: 400.0,
+              decoration: const BoxDecoration(
+                  color:   Color(0xff141b22),
+                  borderRadius: BorderRadius.all(Radius.circular(20))),
+              margin: const EdgeInsets.symmetric(vertical: 40.0, horizontal: 0.0),
+              child: Column(
+                children: <Widget>[
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Container(
+                        padding: const EdgeInsets.all(20.0),
+                        child: Text(
+                          'Temperatura'.toUpperCase(),
+                          style: const TextStyle(
+                            color: Color(0xFFF89411),
+                            fontSize: 36.0,
+                            fontWeight: FontWeight.w700
+                          ),
+                        ),
+                      ),
+                    ]
+                  )
+                ],
+              ),
+            ),
+          ];
+  }
+  List <Widget> get _cardSwitches {
+    return <Widget>  [
+               Container(
+                width: 300.0,
+                height: 400.0,
+                decoration: const BoxDecoration(
+                  color:   Color(0xff141b22),
+                  borderRadius: BorderRadius.all(Radius.circular(20))),
+                child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: LiteRollingSwitch(
+                  value: true,
+                  width: 100,
+                  textOn: 'ON',
+                  textOff: 'OFF',
+                  colorOn: const Color.fromARGB(204, 5, 161, 26),
+                  colorOff: const Color.fromARGB(255, 35, 49, 56),
+                  iconOn: Icons.emoji_emotions_sharp,
+                  iconOff: Icons.mood_bad_rounded,
+                  textSize: 20.0,
+                  textOnColor: Colors.black,
+                  textOffColor: Colors.white,
+                  animationDuration: const Duration(milliseconds: 50),
+                  onChanged: (bool position) {
+                  print("El Dispositivo esta $position");
+                  },
+                  onDoubleTap: () {},
+                  onSwipe: () {},
+                  onTap: () {},
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: LiteRollingSwitch(
+                  value: true,
+                  width: 100,
+                  textOn: 'ON',
+                  textOff: 'OFF',
+                  colorOn: const Color.fromARGB(204, 5, 161, 26),
+                  colorOff: const Color.fromARGB(255, 35, 49, 56),
+                  iconOn: Icons.emoji_emotions_sharp,
+                  iconOff: Icons.mood_bad_rounded,
+                  textSize: 20.0,
+                  textOnColor: Colors.black,
+                  textOffColor: Colors.white,
+                  animationDuration: const Duration(milliseconds: 50),
+                  onChanged: (bool position) {
+                  print("El Dispositivo esta $position");
+                  },
+                  onDoubleTap: () {},
+                  onSwipe: () {},
+                  onTap: () {},
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: LiteRollingSwitch(
+                  value: true,
+                  width: 100,
+                  textOn: 'ON',
+                  textOff: 'OFF',
+                  colorOn: const Color.fromARGB(204, 5, 161, 26),
+                  colorOff: const Color.fromARGB(255, 35, 49, 56),
+                  iconOn: Icons.emoji_emotions_sharp,
+                  iconOff: Icons.mood_bad_rounded,
+                  textSize: 20.0,
+                  textOnColor: Colors.black,
+                  textOffColor: Colors.white,
+                  animationDuration: const Duration(milliseconds: 50),
+                  onChanged: (bool position) {
+                  print("El Dispositivo esta $position");
+                  },
+                  onDoubleTap: () {},
+                  onSwipe: () {},
+                  onTap: () {},
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: LiteRollingSwitch(
+                  value: true,
+                  width: 100,
+                  textOn: 'ON',
+                  textOff: 'OFF',
+                  colorOn: const Color.fromARGB(204, 5, 161, 26),
+                  colorOff: const Color.fromARGB(255, 35, 49, 56),
+                  iconOn: Icons.emoji_emotions_sharp,
+                  iconOff: Icons.mood_bad_rounded,
+                  textSize: 20.0,
+                  textOnColor: Colors.black,
+                  textOffColor: Colors.white,
+                  animationDuration: const Duration(milliseconds: 50),
+                  onChanged: (bool position) {
+                  print("El Dispositivo esta $position");
+                  },
+                  onDoubleTap: () {},
+                  onSwipe: () {},
+                  onTap: () {},
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ];
+      }
 }
